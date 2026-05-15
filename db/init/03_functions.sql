a66f11a49866716dfe368cac0ef53a5ec12d2b4c
Hubert Myszka <hubert.myszka0@gmail.com>
2026-05-15 14:30:51 +0200

    add functions responsible for filtering products

--- db/init/03_functions.sql
+++ db/init/03_functions.sql
@@ -18,3 +18,55 @@ BEGIN
         LIMIT number_of_best_sellers;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION f_get_category_subtree(f_category_id int)
+RETURNS TABLE (categoryid int)
+AS $$
+BEGIN
+    RETURN QUERY
+    WITH RECURSIVE category_tree AS (
+        SELECT c.categoryid
+        FROM categories c
+        WHERE c.categoryid = f_category_id
+
+        UNION ALL
+
+        SELECT c.categoryid
+        FROM categories c
+        INNER JOIN category_tree ct ON c.parentcategoryid = ct.categoryid
+    )
+    SELECT ct.categoryid
+    FROM category_tree ct;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION f_filter_products(
+    min_price decimal default null,
+    max_price decimal default null,
+    f_category_id int default null,
+    f_attributes_filter jsonb default null
+)
+RETURNS TABLE(
+    productid int,
+    name varchar,
+    price decimal,
+    stock int,
+    attributes jsonb
+ ) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        p.productid,
+        p.name,
+        p.price,
+        p.stock,
+        p.attributes
+    FROM products p
+    WHERE p.isactive = true
+        and (min_price is null or p.price >= min_price)
+        and (max_price is null or p.price <= max_price)
+        and (f_category_id is null or p.categoryid in (
+            SELECT categoryid from f_get_category_subtree(f_category_id)))
+        and (f_attributes_filter is null or p.attributes @> f_attributes_filter);
+END;
+$$ LANGUAGE plpgsql;
